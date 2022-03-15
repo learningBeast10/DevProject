ef7c1a6cab2679949af02b33bae40e3fd469777e
learingBeast10 <vishakanvanangamudi@gmail.com>
2022-03-15 12:07:50 -0400

    Second Commit

--- src/MainMethod.java
+++ src/MainMethod.java
@@ -3,6 +3,7 @@ public class MainMethod {
 
 	public static void main(String[] args) {
 		System.out.println("First Project in Git");
+		System.out.println("First Project in Git - added by First Guy");
 	}
 
 }
